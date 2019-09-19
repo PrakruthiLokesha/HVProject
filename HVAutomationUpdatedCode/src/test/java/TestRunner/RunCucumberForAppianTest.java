e91ef0012397c09e1bc758801b00819b417bc92d
srishakthim <srishakthim@VURAM-2018-0332.vuram.com>
2019-09-19 16:09:02 +0530

    16:08

--- HVAutomationUpdatedCode/src/test/java/TestRunner/RunCucumberForAppianTest.java
+++ HVAutomationUpdatedCode/src/test/java/TestRunner/RunCucumberForAppianTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\srishakthim\\git\\repository\\HVAutomationUpdatedCode\\src\\test\\java\\Test_cases", glue = {
+@CucumberOptions(features = "/HVAutomationUpdatedCode/src/test/java/Test_cases", glue = {
     "com.appiancorp.ps.cucumber"}, plugin = {"pretty"}, tags = "@LinkRequest" )
 public class RunCucumberForAppianTest {
 }
